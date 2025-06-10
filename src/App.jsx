806e68e4026867dafb718ec038b2b481b82f4f89
AaryanBasnet <basnetaryan1011@gmail.com>
2025-06-10 22:04:54 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-amber-900">test tailwind</div>
+    </>
+  );
+}
+
+export default App;
