1c73ca694125e846e4cbf2d4fc5008e0147c3f59
AaryanBasnet <basnetaryan1011@gmail.com>
2025-06-10 22:33:43 +0545

    Refactor AppRouter to include Routes component and clean up imports

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import LoginPage from "../pages/LoginPage";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-    <Routes>
-        <Route path='/state' element={<StateManage />}>
-
+      <Routes>
+        <Route path="/state" element={<StateManage />}></Route>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
         </Route>
-        <Route element={}
-    </Routes>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
